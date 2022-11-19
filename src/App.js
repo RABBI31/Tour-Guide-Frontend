2813007b17168003e1b630aa79879d4f8e03ede2
rabbi31 <18103380@iubat.edu>
2022-11-20 00:53:33 +0600

    tour added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useEffect} from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import { ToastContainer } from "react-toastify";
@@ -5,16 +6,29 @@ import "react-toastify/dist/ReactToastify.css";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import AddEditTour from './pages/AddEditTour'
+import Header from "./components/Header";
+import { useDispatch } from "react-redux";
+import { setUser } from "./redux/features/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const user = JSON.parse(localStorage.getItem("profile"));
+  useEffect(() => {
+    dispatch(setUser(user));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <BrowserRouter>
       <div className="App">
+      <Header />
       <ToastContainer />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
+        <Route path='/addTour' element={<AddEditTour />} />
+        <Route path='/editTour/:id' element={<AddEditTour />} />
       </Routes>
         
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { getTours } from "../redux/features/tourSlice";
 
 const Home = () => {
+  const {tours, loading} = useSelector(
+    (state) => ({
+      ...state.tour
+    })
+  );
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getTours());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <h3>loading---</h3>;
+  }
   return (
-    <div>Home</div>
-  )
-}
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <MDBRow className="mt-5">
+        {tours.length === 0 && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No Tours Found
+          </MDBTypography>
+        )}
+        <MDBCol>
+          <MDBContainer>
+            <MDBRow className="row-cols-1 row-cols-md-3 g-2">
+              {tours && tours.map((item,index) => <h2>Tour card</h2>)}
+            </MDBRow>
+          </MDBContainer>
+        </MDBCol>
+      </MDBRow>
+    </div>
+  );
+};
+
 
 export default Home
